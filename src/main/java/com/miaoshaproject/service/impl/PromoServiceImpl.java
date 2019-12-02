d41064a1c66f53c0eb211cb6b50e1afd07b25d58
lidingkui <lidingkui168@126.com>
2019-12-02 16:59:03 +0800

    fix  something

--- src/main/java/com/miaoshaproject/service/impl/PromoServiceImpl.java
+++ src/main/java/com/miaoshaproject/service/impl/PromoServiceImpl.java
@@ -2,7 +2,6 @@ package com.miaoshaproject.service.impl;
 
 import com.miaoshaproject.dao.PromoDOMapper;
 import com.miaoshaproject.dataobject.PromoDO;
-import com.miaoshaproject.error.BussinessException;
 import com.miaoshaproject.service.PromoService;
 import com.miaoshaproject.service.model.PromoModel;
 import org.joda.time.DateTime;
@@ -17,37 +16,37 @@ import org.springframework.stereotype.Service;
 @Service
 public class PromoServiceImpl implements PromoService {
 
-    @Autowired
-    PromoDOMapper promoDOMapper;
+	@Autowired
+	PromoDOMapper promoDOMapper;
 
-    @Override
-    public PromoModel getPromoById(Integer itemId) {
-        //获取对应商品的秒杀信息
-        PromoDO promoDO = promoDOMapper.selectByItemId(itemId);
-        PromoModel promoModel = convertFromDataObject(promoDO);
-        if(promoModel == null) {
-            return null;
-        }
-        //判断秒杀活动的当前状态
-        DateTime now = new DateTime();
-        if(promoModel.getStartDate().isAfterNow()){
-            promoModel.setStatus(1);
-        } else if(promoModel.getEndDate().isBeforeNow()) {
-            promoModel.setStatus(3);
-        } else {
-            promoModel.setStatus(2);
-        }
-        return promoModel;
-    }
+	@Override
+	public PromoModel getPromoById(Integer itemId) {
+		//获取对应商品的秒杀信息
+		PromoDO promoDO = promoDOMapper.selectByItemId(itemId);
+		PromoModel promoModel = convertFromDataObject(promoDO);
+		if (promoModel == null) {
+			return null;
+		}
+		//判断秒杀活动的当前状态
+		DateTime now = new DateTime();
+		if (promoModel.getStartDate().isAfterNow()) {
+			promoModel.setStatus(1);
+		} else if (promoModel.getEndDate().isBeforeNow()) {
+			promoModel.setStatus(3);
+		} else {
+			promoModel.setStatus(2);
+		}
+		return promoModel;
+	}
 
-    private PromoModel convertFromDataObject(PromoDO promoDO){
-        if(promoDO == null) {
-            return null;
-        }
-        PromoModel promoModel = new PromoModel();
-        BeanUtils.copyProperties(promoDO,promoModel);
-        promoModel.setStartDate(new DateTime(promoDO.getStartDate()));
-        promoModel.setEndDate(new DateTime(promoDO.getEndDate()));
-        return promoModel;
-    }
+	private PromoModel convertFromDataObject(PromoDO promoDO) {
+		if (promoDO == null) {
+			return null;
+		}
+		PromoModel promoModel = new PromoModel();
+		BeanUtils.copyProperties(promoDO, promoModel);
+		promoModel.setStartDate(new DateTime(promoDO.getStartDate()));
+		promoModel.setEndDate(new DateTime(promoDO.getEndDate()));
+		return promoModel;
+	}
 }
